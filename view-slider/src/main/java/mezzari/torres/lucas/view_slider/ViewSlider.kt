f84c25af2c6594b9023322a64d5f2bc7c4ab2b85
Lucas Torres Mezzari <lucas.mezzari1@gmail.com>
2019-08-31 18:08:31 -0300

    Jitpack plug in

--- view-slider/src/main/java/mezzari/torres/lucas/view_slider/ViewSlider.kt
+++ view-slider/src/main/java/mezzari/torres/lucas/view_slider/ViewSlider.kt
@@ -7,7 +7,6 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import mezzari.torres.lucas.view_slider.R
 
 /**
  * @author Lucas T. Mezzari
